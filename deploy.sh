6dd1ef689b889368f38cca8e6cce4b88ddd0a61e
zombiej <smith3816@gmail.com>
2018-11-18 16:55:25 +0800

    update script

--- deploy.sh
+++ deploy.sh
@@ -1,1 +1,17 @@
-echo "asd"
+echo "dist page..."
+npm run dist
+rm -rf /tmp/dist
+mv dist /tmp/
+
+echo "switch to page branch..."
+git co gh-pages
+mv /tmp/dist/* ./
+rm -rf /tmp/dist
+
+echo "upload..."
+git add .
+git commit -m "update docs"
+git push -f
+
+echo "back to master branch..."
+git co -f master
